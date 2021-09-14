b5a02764126e12f79379d10d4862217710698b03
Jean Meira <jean.meira10@hotmail.com>
2021-09-14 17:04:15 -0300

    Add responsibility

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,21 @@ import luxury from "./assets/images/icon-luxury.svg";
 function App() {
   return (
     <div className="App">
-      <div className="content">
-        <Card tittle="Sedans" icon={sedan} color="orange">
-          Choose a sedan for its affordability and excellent fuel economy. Ideal
-          for cruising in the city or on your next road trip.
-        </Card>
-        <Card tittle="SUVs" icon={suv} color="dark">
-          Take an SUV for its spacious interior, power, and versatility. Perfect
-          for your next family vacation and off-road adventures.
-        </Card>
-        <Card tittle="Luxury" icon={luxury} color="very-dark">
-          Cruise in the best car brands without the bloated prices. Enjoy the
-          enhanced comfort of a luxury rental and arrive in style.
-        </Card>
+      <div className="conteiner">
+        <div className="content">
+          <Card tittle="Sedans" icon={sedan} color="orange">
+            Choose a sedan for its affordability and excellent fuel economy.
+            Ideal for cruising in the city or on your next road trip.
+          </Card>
+          <Card tittle="SUVs" icon={suv} color="dark">
+            Take an SUV for its spacious interior, power, and versatility.
+            Perfect for your next family vacation and off-road adventures.
+          </Card>
+          <Card tittle="Luxury" icon={luxury} color="very-dark">
+            Cruise in the best car brands without the bloated prices. Enjoy the
+            enhanced comfort of a luxury rental and arrive in style.
+          </Card>
+        </div>
       </div>
     </div>
   );
